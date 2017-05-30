123a712240ca38c9c5f534335859f0a43e909301
rcc <rcc>
2017-05-30 12:11:21 -0700

    Signed-off-by: rcc <rcc>

---
+++ Lab/Lab 053017/markSort_DatabaseSort_StructDynMemory/Data.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Edwin Leon
+ * Created on May 30, 2017, 11:43 AM
+ * Purpose:  Mark Sort implemented with a database approach
+ *           and a structure with Dynamic Memory
+ */
+
+#ifndef DATA_H
+#define DATA_H
+
+const int SIZE=200;
+
+struct Data{
+    int utilize;
+    int *array;
+    int *index;
+};
+
+
+#endif /* DATA_H */
+
